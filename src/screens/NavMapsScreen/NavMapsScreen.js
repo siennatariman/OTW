947e1486ee7f52994bf3f872d8a0e40bc5b21c98
siennatariman <tariman.sienna@gmail.com>
2022-08-06 15:32:23 +0800

    removed meta data from android manifest

--- src/screens/NavMapsScreen/NavMapsScreen.js
+++ src/screens/NavMapsScreen/NavMapsScreen.js
@@ -2,12 +2,21 @@ import { View, Text } from 'react-native'
 import React from 'react'
 import MapView from 'react-native-maps';
 
+
 const NavMapsScreen = () => {
   return (
-    <View style={{flex: 1}}>
-      <MapView 
-          style={{flex: 1}}  />
-    </View> 
+  //   <MapView
+  //   initialRegion={{
+  //     latitude: 37.78825,
+  //     longitude: -122.4324,
+  //     latitudeDelta: 0.0922,
+  //     longitudeDelta: 0.0421,
+  //   }}
+  // /> 
+
+  <View>
+      <Text>NHello!</Text>
+  </View>
   )
 }
 
